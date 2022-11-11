ddf17495933193aeeb34b4531df46f9f32a94d1b
Daniel Veloso <danielferreiraveloso@hotmail.com>
2022-11-10 23:10:03 -0300

    refactoring ToDo_Repository

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -2,32 +2,18 @@ import 'package:dio/dio.dart';
 import '../models/todo_model.dart';
 
 class ToDoRepository{
-
   Dio? dio;
   final url = 'https://jsonplaceholder.typicode.com/todos';
 
   ToDoRepository([Dio? client]){
-    if(client == null){
-      this.dio = Dio();
-    } else {
-      this.dio = client;
-    }
+    dio = client ?? Dio();
   }
 
-  Future fetchToDos() async{
-    
+  Future fetchToDos() async{ 
     final response = await dio!.get(url);
     final list = response.data as List;
 
-    List<ToDoModel> toDos = [];
-
-    for(var json in list){
-      final toDo = ToDoModel.fromJson(json);
-      toDos.add(toDo);
-    }
-
-    return toDos;
-    
+    return list.map((json) => ToDoModel.fromJson(json)).toList();
   }
 
 }
